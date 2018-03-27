c901ae290982dcb9ca7e4da14dc059abba862362
MLR-SUN <sun.ios06444@gmail.com>
2018-03-27 10:13:51 +0800

    設定button1_click的function，點擊按鈕跑出提示訊息

--- app/src/main/java/com/example/admin/ch5_4_3/MainActivity.java
+++ app/src/main/java/com/example/admin/ch5_4_3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.admin.ch5_4_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void button1_click(View view){
+        Toast.makeText(this, "Hello, button1", Toast.LENGTH_LONG).show();
     }
 }
